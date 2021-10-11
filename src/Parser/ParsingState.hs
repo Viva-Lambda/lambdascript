c21de8a5ca91e3e822d5a61d174bb5dc69612823
Viva Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2021-10-11 08:44:12 +0200

    Some plumbing, needs polishing
    Some plumbing of expressions had been done
    Now all expressions needs to be typed.
    Function body type checking needs to be
    done with unions. For example if a function
    body has 3 lines producing int, float, int
    and the codomain of the function is
    int since (int | float | int) produces true
    we can say at compile time that this function
    might be valid, so we leave it alone. If
    the body is something like (string, string, string)
    we say this function is not valid.
    There is no possibility that it can produce
    int. We also check the type of return value during
    the parsing and compare it with codomain of the
    function

--- src/Parser/ParsingState.hs
+++ src/Parser/ParsingState.hs
@@ -5,12 +5,37 @@ import qualified Data.Map as DMap
 import TypeSystem.TypeIO
 import TypeSystem.TypeChecker
 import TypeSystem.TypeRules
+import TypeSystem.TypeError
+
+import Expression.Literal
+import Expression.Identifier
+import Expression.Expression
 
 type ParsingState = DMap.Map String Typed
 
 emptyState :: ParsingState
 emptyState = DMap.empty
 
--- addLit2PState :: Literal -> ParsingState -> ParsingState 
--- addLit2PState lit pstate =
---    let (ValueType s tname info) = fromLiteral2ValueType lit
+addLit2PState :: Literal -> ParsingState -> ParsingState 
+addLit2PState lit pstate =
+    let (ValueType s tname) = fromLiteral2ValueType lit
+        tval = ValueTyper (ValueType s tname)
+        ps = DMap.insert s tval pstate
+    in ps
+
+addIde2PState :: Identifier -> ParsingState -> ParsingState 
+addIde2PState ids pstate =
+    let (ValueType s tname) = fromIdentifier2ValueType ids
+        tval = ValueTyper (ValueType s tname)
+    in case DMap.lookup s pstate of
+            Nothing -> DMap.insert s tval pstate
+            Just t -> error $ show ( NameAlreadyBound s tname t)
+            
+addProcDef2PState :: ProcedureDefinition -> ParsingState -> ParsingState
+addProcDef2PState pd pstate =
+    let (IdExpr (VName pname _) tn _) = procname pd
+        ptype = FunctionTyper $ fromProcDef2FnType pd
+    in case DMap.lookup pname pstate of
+            Nothing -> DMap.insert pname ptype pstate
+            Just t -> error $ show ( NameAlreadyBound pname tn t)
+
---
+++ src/TypeSystem/TypeError.hs
@@ -0,0 +1,17 @@
+-- parsing state module
+module TypeSystem.TypeError where
+
+import TypeSystem.TypeRules
+import Expression.Identifier
+
+data TypeError = NameAlreadyBound String TypeName Typed
+
+instance Show TypeError where
+    show (NameAlreadyBound idn (VName tn info) t) =
+        let info2 = getTypeInfo t
+            msg1 = "TypeError :: Name already bound to another type"
+            msg2 = " \nName: " ++ idn
+            msg3 = " \nPrevious declaration: " ++ show info2
+            msg4 = " \nCurrent declaration: " ++ show info
+        in error $ msg1 ++ msg2 ++ msg3 ++ msg4
+
